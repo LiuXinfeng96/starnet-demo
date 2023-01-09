b26a767d7bc314f1a64465a65fdda427b3e8e192
liuxinfeng <liuxinfeng96@163.com>
2023-01-09 11:05:13 +0800

    fix the arm64v8 deploy shell

--- deploy_arm64v8.sh
+++ deploy_arm64v8.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 path=`pwd`
 
-i=$(docker images | grep "starnet-demo" | awk '{print $1}')
+i=$(docker images | grep "arm64v8/starnet-demo" | awk '{print $1}')
 if test -z $i; then
 echo "not found the docker image, start build image..."
 docker build -f ./DockerFile.arm64v8 -t arm64v8/starnet-demo:v0.9.0 ../starnet-demo
 fi
 
-i=$(docker images | grep "starnet-demo" | awk '{print $1}')
+i=$(docker images | grep "arm64v8/starnet-demo" | awk '{print $1}')
 if test -z $i; then
 echo "build image error, exit shell!"
 exit
 fi
 
-c=$(docker ps -a | grep "starnet-mysql" | awk '{print $1}')
+c=$(docker ps -a | grep "arm64v8-starnet-mysql" | awk '{print $1}')
 if test -z $c; then
 echo "not found the mysql server, start mysql server..."
 docker run -d \
     -p 3306:3306 \
     -e MYSQL_ROOT_PASSWORD=123456 \
     -e MYSQL_DATABASE=chainmaker \
-    --name arm64v8/starnet-mysql \
+    --name arm64v8-starnet-mysql \
     --restart always \
     arm64v8/mysql:8.0
 echo "waiting for database initialization..."
 sleep 20s
-docker logs --tail=10 starnet-mysql
+docker logs --tail=10 arm64v8-starnet-mysql
 fi
 
-i=$(docker ps -a | grep "starnet-demo" | awk '{print $1}')
+i=$(docker ps -a | grep "arm64v8-starnet-demo" | awk '{print $1}')
 if test ! -z $i; then
 echo "the server container already exists, delete..."
-docker rm -f starnet-demo
+docker rm -f arm64v8-starnet-demo
 fi
 
 echo "start starnet demo server..."
@@ -41,19 +41,19 @@ docker run -d \
 -v $path/conf:/starnet-demo/conf \
 -v $path/log:/starnet-demo/log \
 -v $path/crypto-config:/starnet-demo/crypto-config \
---name starnet-demo \
+--name arm64v8-starnet-demo \
 --restart always \
 arm64v8/starnet-demo:v0.9.0 \
 bash -c "cd src&&./starnet-demo -config ../conf/system_config.yaml"
 sleep 2s
-docker logs starnet-demo
+docker logs arm64v8-starnet-demo
 echo "the starnet demo server has been started!"
 
 
-i=$(docker ps -a | grep "starnet-web" | awk '{print $1}')
+i=$(docker ps -a | grep "arm64v8-starnet-web" | awk '{print $1}')
 if test ! -z $i; then
 echo "the web container already exists, delete..."
-docker rm -f starnet-web
+docker rm -f arm64v8-starnet-web
 fi
 
 echo "start starnet demo web server..."
@@ -62,7 +62,7 @@ docker run -d \
 -p 80:80 \
 -v $path/web/conf.d:/etc/nginx/conf.d \
 -v $path/web/resources:/usr/share/nginx/resources \
---name arm64v8/starnet-web \
+--name arm64v8-starnet-web \
 --restart always \
 arm64v8/nginx:1.23.3
 echo "the starnet demo web server has been started!"
